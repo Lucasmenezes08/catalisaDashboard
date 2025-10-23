71afe75f0759e7681dd98d42f7ba4a1edef1e38f
Lucasmenezes08 <lms4@cesar.school>
2025-10-22 21:46:21 -0300

    :lipstick: style: estilizar inicialmente a navbar.

--- cataliza/src/components/layout/Navbar.tsx
+++ cataliza/src/components/layout/Navbar.tsx
@@ -4,13 +4,13 @@ import { NavLink } from "react-router-dom";
 
 export default function NavBar (){
     return (
-        <section className="flex flex-row justify-center items-center">
-            <section className="justify-around items-center">
+        <section className="bg-gray-200 w-full py-4 px-12 mb-10">
+            <section className="flex flex-row justify-between items-center">
                 <NavLink to={"/"}>Use nossos serviços</NavLink>
                 <NavLink to={"/"}>Inicio</NavLink>
-                <section className="flex flex-row ">
-                    <NavLink to={"/Login"}>Login</NavLink>
-                    <IoPersonCircleOutline size={25}/>
+                <section className="flex flex-row justify-center items-center gap-2 ">
+                    <NavLink to={"/Login"} className="text-[#0F0FA6]">Login</NavLink>
+                    <IoPersonCircleOutline size={40}/>
                 </section>
             </section>
         </section>
