95744c78fa772755c6318721ac289cd3877c7126
Ricardo Freitas <ricardo.sergio.pff@gmail.com>
2025-10-27 17:38:07 -0300

    refactor: changed name field to be accepted as nullable

--- catalisabackend/src/main/java/com/cesarschool/catalisabackend/user/UserApp.java
+++ catalisabackend/src/main/java/com/cesarschool/catalisabackend/user/UserApp.java
@@ -19,7 +19,7 @@ public class UserApp implements Serializable {
     private Long id;
 
     @NotBlank
-    @Column(nullable = false, unique = true, length = 50)
+    @Column(unique = true, length = 50)
     private String name;
 
     @Email
