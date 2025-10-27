f4322cf57d684a5f922517f86b5e6f09da198ad4
Ricardo Freitas <ricardo.sergio.pff@gmail.com>
2025-10-27 16:08:45 -0300

    feat: backend for catalisa created

---
+++ catalisabackend/src/main/java/com/cesarschool/catalisabackend/CatalisabackendApplication.java
@@ -0,0 +1,13 @@
+package com.cesarschool.catalisabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalisabackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatalisabackendApplication.class, args);
+    }
+
+}
