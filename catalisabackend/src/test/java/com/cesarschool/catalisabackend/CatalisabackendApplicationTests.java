e2e991ac6cfc7bce86757991f7ad969fa3c065fc
Ricardo Freitas <ricardo.sergio.pff@gmail.com>
2025-10-28 20:48:01 -0300

    feat: build backend

---
+++ catalisabackend/src/test/java/com/cesarschool/catalisabackend/CatalisabackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesarschool.catalisabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalisabackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
