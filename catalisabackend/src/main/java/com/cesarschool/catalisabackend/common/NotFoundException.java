fea81f436d83ef6bb86ee250d9bcce3b71365e1d
Ricardo Freitas <ricardo.sergio.pff@gmail.com>
2025-10-28 20:37:13 -0300

    refactor: refactoring backend

--- catalisabackend/src/main/java/com/cesarschool/catalisabackend/common/NotFoundException.java
+++
@@ -1,5 +0,0 @@
-package com.cesarschool.catalisabackend.common;
-
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) { super(message); }
-}
--- catalisabackend/src/main/java/com/cesarschool/catalisabackend/user/dto/UserResponse.java
+++
@@ -1,7 +0,0 @@
-package com.cesarschool.catalisabackend.user.dto;
-
-public record UserResponse(
-        Long id,
-        String name,
-        String email
-) {}
