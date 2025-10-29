509a43b980e3c621189ab6b866292bec78322657
Gabriel Aniceto <gabriel.aniceto@hotmail.com>
2025-10-29 16:00:36 -0300

    adicionando StringUtils

--- catalisabackend/src/main/java/com/cesarschool/catalisabackend/models/product/ProductService.java
+++ catalisabackend/src/main/java/com/cesarschool/catalisabackend/models/product/ProductService.java
@@ -1,4 +1,23 @@
 package com.cesarschool.catalisabackend.models.product;
 
+import org.springframework.stereotype.Service;
+import com.cesarschool.catalisabackend.models.utils.StringUtils;
+
+@Service
 public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+    public Product include(Product product){
+        if(){
+
+        }
+        if(){
+
+        }
+        return productRepository.save(product);
+    }
 }
