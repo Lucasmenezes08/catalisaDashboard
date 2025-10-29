79d6a636fe005823ac82f63fd84d8789a07d299e
Lucasmenezes08 <lms4@cesar.school>
2025-10-29 12:02:05 -0300

    :sparkles: Implementar hover de explicações por pesquisa.

---
+++ catalisa/src/components/dashboard/grafico/ExplicacaoModal.tsx
@@ -0,0 +1,7 @@
+export default function ExplicacaoModal ({mensagem}:any){
+    return (
+        <section className="w-full flex items-center justify-center bg-white text-sm rounded-2xl border border-black px-1 py-3 transition ease-in-out delay-200">
+            <h1 className="text-justify break-after-all w-[80%]">{mensagem}</h1>
+        </section>
+    )
+}
--- catalisa/src/components/dashboard/grafico/ProporcaoDeRespostas.tsx
+++ catalisa/src/components/dashboard/grafico/ProporcaoDeRespostas.tsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
 import { CiCircleInfo } from "react-icons/ci";
+import ExplicacaoModal from "./explicacaoModal";
 
 export default function PropocaoDeRespostas(){
+
+    const [isHovered , setIsHovered] = useState(false);
     return (
         <section className="w-full h-full flex flex-col rounded-xl border border-solid border-black">
             <section className="relative flex justify-center items-center p-4 border-b border-black">
                 <h2 className="text-md font-medium text-black uppercase">Proporção de Respostas</h2>
-                <CiCircleInfo className="absolute right-4 text-blue-700" size={25}/>
+                <CiCircleInfo className="absolute right-4 text-blue-700" size={30} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}/>
+                {isHovered && 
+                    <section className="absolute pr-15 pt-15">
+                        <ExplicacaoModal mensagem={"o cálculo do NPS é feito a partir da porcentagem (%) dos promotores menos(-) a porcentagem (%) dos detratores"}/>
+                    </section>
+                }
             </section>
 
             <section className="flex-1 p-4 min-h-0">
