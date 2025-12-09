e85339ce4ee4e1ecfa4b96033e3ff933c742e96f
eric-albuquer <ega2@cesar.school>
2025-12-09 00:25:51 -0300

    Corrigir telas

--- catalisa/src/components/dashboard/grafico/Grafico.tsx
+++ catalisa/src/components/dashboard/grafico/Grafico.tsx
@@ -6,7 +6,7 @@ import TaxaDeResposta from "./TaxaDeResposta";
 
 export default function Grafico() {
   return (
-    <section className="w-full min-h-screen bg-white border border-solid border-black rounded-xl p-2 mt-2">
+    <section className="w-full h-full bg-white border border-solid border-black rounded-xl p-2 mt-2">
       <section className="h-full grid grid-cols-1 md:grid-cols-3 grid-rows-2 gap-y-3">
         
         <DistribuicaoResposta />
