f663a720a041cd604820407c56fa5edfb3a2fa3f
Gabriel Aniceto <gabriel.aniceto@hotmail.com>
2026-03-25 21:04:27 -0300

    adicionando detalhamento nas configurações do swagger

--- catalisabackend/src/main/java/com/cesarschool/catalisabackend/config/OpenApiConfig.java
+++ catalisabackend/src/main/java/com/cesarschool/catalisabackend/config/OpenApiConfig.java
@@ -1,10 +1,15 @@
 package com.cesarschool.catalisabackend.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -13,11 +18,24 @@ public class OpenApiConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("API Catalisa")
-                        .version("1.0.0")
-                        .description("Documentação da API do projeto Catalisa")
+                        .title("Catalisa API")
+                        .version("v2")
+                        .description("""
+                                API responsável pelo gerenciamento de usuários, produtos, pesquisas,
+                                consumos e indicadores analíticos do projeto Catalisa.
+                                
+                                Principais módulos:
+                                - Users
+                                - Products
+                                - Pesquisas
+                                - Consumos
+                                - Dashboard
+                                """)
                         .contact(new Contact()
                                 .name("Equipe Catalisa")
-                                .email("gasab@cesar.school")));
+                                .email("gasab@cesar.school"))
+                        .license(new License()
+                                .name("Uso acadêmico / interno")))
+                .components(new Components());
     }
 }
